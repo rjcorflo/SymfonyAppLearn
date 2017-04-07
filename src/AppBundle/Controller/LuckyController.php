715de1790b7b83d7499a0e00c80c75780f94f2c2
rjcorflo <rj.corchero@gmail.com>
2017-04-07 23:38:03 +0200

    [DEV] Add LuckyController extending from controller. Use twig templating system.

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -1,9 +1,11 @@
 <?php
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class LuckyController
+class LuckyController extends Controller
 {
     /**
      * @Route("/lucky/number")
@@ -12,8 +14,6 @@ class LuckyController
     {
         $number = mt_rand(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', ['number' => $number]);
     }
 }
